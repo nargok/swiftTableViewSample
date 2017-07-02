c88bd6b877492aa16c04b8b58bf9224aec72e67a
後閑諒一 <houxianliangyi@gokanryouichinoMacBook.local>
2017-07-02 09:51:13 +0900

    Initial Commit

---
+++ tableViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewSample
+//
+//  Created by 後閑諒一 on 2017/07/02.
+//  Copyright © 2017年 後閑諒一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
